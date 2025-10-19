e2d30b1e208cc01db93304b1a07f469ebbf1ebb6
Bristo Biju <blasterbiju@gmail.com>
2025-10-19 20:26:03 +0530

    Remove root example files as requested

--- 5_reverse_string.js
+++
@@ -1,8 +0,0 @@
-// 5_reverse_string.js
-function reverseString(s) {
-  return String(s).split('').reverse().join('');
-}
-
-console.log("reverseString('hello') ->", reverseString('hello'));
-
-module.exports = { reverseString };
