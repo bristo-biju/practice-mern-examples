f4955c5a2610d77a1f4c21bdddbe4746996782d7
Bristo Biju <blasterbiju@gmail.com>
2025-10-19 20:25:41 +0530

    Remove root example files as requested

--- 2_sum_array.js
+++
@@ -1,9 +0,0 @@
-// 2_sum_array.js
-function sumArray(arr) {
-  return arr.reduce((acc, n) => acc + n, 0);
-}
-
-console.log('sumArray([1,2,3]) ->', sumArray([1, 2, 3]));
-console.log('sumArray([]) ->', sumArray([]));
-
-module.exports = { sumArray };
